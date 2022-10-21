63b5472ba0fab34d54f25214da5809e3f408c0c2
Alona <103571003+ARomanii@users.noreply.github.com>
2022-10-21 23:41:09 +0300

    ДЗ 1 ч.4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './Transactions/Transactions';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './Transactions/transactions.json';
 
 
 export const App = () => {
@@ -19,6 +21,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/Transactions/Transactions.jsx
@@ -0,0 +1,37 @@
+import './Transactions.module.css';
+import PropTypes from 'prop-types';
+
+function createColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+export const TransactionHistory = ({ items }) => {
+    return (
+    <table>
+      <thead>
+        <tr style={{ backgroundColor: createColor() }}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} style={{ backgroundColor: createColor() }}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
