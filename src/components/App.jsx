7f2a1c1abbf41c422089adcffc05468f2d996c92
Alona <103571003+ARomanii@users.noreply.github.com>
2022-10-21 22:59:07 +0300

    ДЗ 1 ч. 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
 
 
 export const App = () => {
@@ -16,6 +18,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
